5364a234d9e65a43b192e95541c75660ebeaf435
Tobias321 <tgrothe@fh-bielefeld.de>
2017-05-26 20:48:21 +0200

    Login verbessert/angepasst

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import { Page } from 'ionic/ionic';
 
 import { TabsPage } from '../tabs/tabs';
@@ -38,20 +38,35 @@ export class Login {
 
   login() {
     var link = 'https://moveit-backend.herokuapp.com/login';
+
+    /*
     var data = { username: this.loginVars.username, password: this.loginVars.password };
 
-    this.http.post(link, data)
-      .subscribe(data => {
-        this.data.response = data["_body"];
+    let headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    let options = new RequestOptions({
+      headers: headers
+    });
+    let body = JSON.stringify({
+      username: this.loginVars.username,
+      password: this.loginVars.password
+    });
+    */
+
+    this.http.post(link, {username: this.loginVars.username, password: this.loginVars.password})
+      .map(response => response.json())
+      .subscribe(response => {
+        if (response.message === 'Login erfolgreich') {
+          this.navCtrl.setRoot(TabsPage);
+        } else {
+
+        }
       }, error => {
         console.log("Oooops!");
       });
 
-    if (this.data.response.indexOf('erfolgreich') >= 0) {
-      this.navCtrl.setRoot(TabsPage);
-    } else if (this.data.response.indexOf('fehlgeschlagen') >= 0) {
 
-    }
 
   }
 
