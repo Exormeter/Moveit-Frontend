1ac0a3cd1f265cf729de17cb616603027a5ea265
TwentyOneLunex <krassover@web.de>
2017-06-09 13:34:54 +0200

    Registration läuft

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Page } from 'ionic/ionic';
 
-/**
- * Generated class for the Register page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { Login } from '../login/login';
+
 @IonicPage()
 @Component({
   selector: 'page-register',
   templateUrl: 'register.html',
 })
+
 export class Register {
 
   registerVars = {
@@ -26,7 +25,10 @@ export class Register {
     passwordCheck: ''
   };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private alertCtrl: AlertController, 
+              public navCtrl: NavController, 
+              public navParams: NavParams, 
+              public http: Http) {
   }
 
   ionViewDidLoad() {
@@ -34,17 +36,49 @@ export class Register {
   }
 
   createAccount() {
+    var link = 'https://moveit-backend.herokuapp.com/signup';
 
+    this.http.post(link, {
+      firstname: this.registerVars.firstname,
+      lastname: this.registerVars.lastname,
+      email: this.registerVars.email,
+      birthdate: this.registerVars.birthdate,
+      sex: this.registerVars.sex,
+      username: this.registerVars.username,
+      password: this.registerVars.password,
+      passwordCheck: this.registerVars.passwordCheck
+    })
+      .map(response => response.json())
+      .subscribe(response => {
+        if (response.message === 'User Registration succesful') {
+          this.presentAlert('Erfolgreich', 'Glückwunsch, Sie können sich jetzt anmelden');
+        } else if (response.message === 'Missing credentials') {
+          this.presentAlert('Fehlgeschlagen', 'Benutzername oder Passwort fehlt');
+        } else if (response.message === 'User Already Exists') {
+          this.presentAlert('Fehlgeschlagen', 'Benutzername bereits vergeben.');
+        }
+      }, error => {
+        console.log("Oooops!");
+      });
   }
 
   resetInputs() {
     this.registerVars.firstname = '',
-    this.registerVars.lastname = '',
-    this.registerVars.email = '',
-    this.registerVars.birthdate = '',
-    this.registerVars.sex = '',
-    this.registerVars.username = '',
-    this.registerVars.password = '',
-    this.registerVars.passwordCheck = ''
+      this.registerVars.lastname = '',
+      this.registerVars.email = '',
+      this.registerVars.birthdate = '',
+      this.registerVars.sex = '',
+      this.registerVars.username = '',
+      this.registerVars.password = '',
+      this.registerVars.passwordCheck = ''
+  }
+
+  presentAlert(title, subTitle) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: subTitle,
+      buttons: ['Okay']
+    });
+    alert.present();
   }
 }
