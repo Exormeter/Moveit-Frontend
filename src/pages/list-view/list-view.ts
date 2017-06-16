cc39ca8bf1cd677ae15ad75d841e0139b91e2d89
Nils <nils.kirchhof@fh-bielefeld.de>
2017-06-16 12:32:45 +0200

    ListView weitergemacht

--- src/pages/list-view/list-view.ts
+++ src/pages/list-view/list-view.ts
@@ -1,6 +1,8 @@
+import {EventView} from '../event-view/event-view';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {ModalController, IonicPage,  NavController,  NavParams} from 'ionic-angular';
 import { RestService } from "../../services/restService";
+import { MyEvent } from "../../models/event";
 
 /**
  * Generated class for the ListView page.
@@ -15,35 +17,39 @@ import { RestService } from "../../services/restService";
 })
 export class ListView {
 
-  items1: any;
-  items2: any;
+  myEvents: MyEvent[] = new Array();
+  myEventsSub: MyEvent[] = new Array();
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public restService: RestService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public restService: RestService, public modalCtrl: ModalController) {
+  }
+
+
+  itemSelected(event: MyEvent){
+    let eventViewer = this.modalCtrl.create(EventView,{event: event})
+    eventViewer.present();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ListView');
     this.restService.getMyEvents()
-      .subscribe(response => {
-        var stringList = [];
-        response.forEach(element => {
-          stringList.push(JSON.stringify(element));
-        });
-        this.items1 = stringList;
+    .subscribe(response => {
+      response.forEach(element => { 
+        this.myEvents.push(new MyEvent(element._id, element.createdAt, element.creator, element.title, element.longitude,
+        element.latitude, element.start, element.__v, element.subscriber, element.keywords));
       }, error => {
         console.log("Oooops!");
       });
+    });
 
       this.restService.getMyEventSubscriber()
       .subscribe(response => {
-        var stringList = [];
-        response.forEach(element => {
-          stringList.push(JSON.stringify(element));
-        });
-        this.items2 = stringList;
+      response.forEach(element => { 
+        this.myEventsSub.push(new MyEvent(element._id, element.createdAt, element.creator, element.title, element.longitude,
+        element.latitude, element.start, element.__v, element.subscriber, element.keywords));
       }, error => {
         console.log("Oooops!");
       });
+    });
   }
 
 }
