1db47f6ec963acd451d7c55043fbe58781608281
Tobias321 <tgrothe@fh-bielefeld.de>
2017-07-14 01:46:26 +0200

    list-view angepasst

--- src/pages/list-view/list-view.ts
+++ src/pages/list-view/list-view.ts
@@ -59,7 +59,7 @@ export class ListView {
       .subscribe(response => {
         response.forEach(element => {
           this.myEvents.push(new MyEvent(element._id, element.createdAt, element.creator, element.title, element.longitude,
-            element.latitude, element.start, element.__v, element.picture, element.subscriber, element.keywords));
+            element.latitude, new Date(element.starttimepoint).toString(), element.__v, element.picture, element.subscriber, element.keywords));
         }, error => {
           console.log("Oooops!");
         });
@@ -69,7 +69,7 @@ export class ListView {
       .subscribe(response => {
         response.forEach(element => {
           this.myEventsSub.push(new MyEvent(element._id, element.createdAt, element.creator, element.title, element.longitude,
-            element.latitude, element.start, element.__v, element.picture, element.subscriber, element.keywords,
+            element.latitude, new Date(element.starttimepoint).toString(), element.__v, element.picture, element.subscriber, element.keywords,
             Math.round(element.distA / 10.0) / 100.0));
         }, error => {
           console.log("Oooops!");
@@ -96,71 +96,6 @@ export class ListView {
         return (item.$title.toLowerCase().indexOf(val.toLowerCase()) > -1);
       })
     }
-  }
-
-  getDistance(lat: number, lng: number) {
-    var geolocation = (function () {
-      'use strict';
-
-      var geoposition;
-      var options = {
-        maximumAge: 1000,
-        timeout: 15000,
-        enableHighAccuracy: false
-      };
-
-      function _onSuccess(callback, position) {
-        //console.log('LAT: ' + lat + 'LNG: ' +lng);
-        //console.log('LAT: ' + position.coords.latitude + ' - LON: ' +  position.coords.longitude);
-        getDistanceFromLatLonInKm(lat, lng, position.coords.latitude, position.coords.longitude);
-        geoposition = position
-        callback();
-      };
-
-      function _onError(callback, error) {
-        console.log(error)
-        callback();
-      };
-
-      function _getLocation(callback) {
-        navigator.geolocation.getCurrentPosition(
-          _onSuccess.bind(this, callback),
-          _onError.bind(this, callback),
-          options
-        );
-      }
-
-      function deg2rad(deg) {
-        return deg * (Math.PI / 180)
-      }
-
-
-      function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-
-        var R = 6371; // Radius of the earth in km
-        var dLat = deg2rad(lat2 - lat1);  // deg2rad below
-        var dLon = deg2rad(lon2 - lon1);
-        var a =
-          Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-          Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-          Math.sin(dLon / 2) * Math.sin(dLon / 2)
-          ;
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        var d = R * c; // Distance in km
-        console.log("Distanz:");
-        console.log(d);
-        return d;
-      }
-
-      return {
-        location: _getLocation
-      }
-
-    }());
-
-    geolocation.location(function () {
-      console.log('finished, loading app.');
-    });
   }
 
   presentAlert(title, subTitle) {
