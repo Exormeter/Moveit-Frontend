c75aac70ee6cab9cccdf73819b47d971f83d31b7
TwentyOneLunex <krassover@web.de>
2017-09-04 15:38:30 +0200

    fehlende Übersetzung gefunden

--- src/services/pushService.ts
+++ src/services/pushService.ts
@@ -1,34 +1,34 @@
 import { Injectable } from '@angular/core';
-import {Response, Http, Headers} from '@angular/http';
-import {Observable} from 'rxjs/Rx';
+import { Response, Http, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import { User } from "../models/user";
 
 @Injectable()
-export class PushService{
+export class PushService {
 
     apikey: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmMDNhYWU1NC03ODYwLTRiNTgtODA1Ny0wOTU5MTM4Yjg3YjIifQ.JQW--Jokpf7HBxE4FJD1m94mqgVIJQPj1ZUnIe28db8';
     baseurl: string = 'https://api.ionic.io';
     profile: string = 'moveit';
     headers: Headers;
 
-    constructor(private http: Http){
+    constructor(private http: Http) {
         this.headers = new Headers();
         this.headers.append('Authorization', 'Bearer ' + this.apikey);
         this.headers.append('Content-Type', 'application/json');
     }
 
-    sendPush(token: string, userName: string, event: string): Observable<any>{
+    sendPush(token: string, userName: string, event: string): Observable<any> {
         let url: string = this.baseurl + '/push/notifications';
-        var payload =  {
+        var payload = {
             tokens: [token],
             profile: 'moveit',
             notification: {
-                message: userName + ' nimmt an deinem Move '+ event + ' teil!'
+                message: userName + ' is joining your event: ' + event
             }
         }
-        let response: Observable<any> = this.http.post(url, JSON.stringify(payload), {headers: this.headers})
-        .map(response => response.json());
+        let response: Observable<any> = this.http.post(url, JSON.stringify(payload), { headers: this.headers })
+            .map(response => response.json());
         return response;
     }
 }
